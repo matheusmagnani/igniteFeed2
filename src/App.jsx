6246773adb923261bbdbeee185afd174f6d295f3
Matheus Magnani <matheusmagnani@MacBook-Air-de-Matheus.local>
2023-10-25 21:16:38 -0300

    Create Sidebar component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar"
 
+import styles from "./App.module.css";
 import './global.css';
 
 export function App() {
@@ -8,6 +10,14 @@ export function App() {
   return (
     <>
       <Header/>
+      <div className={styles.wrapper}>
+        <aside>
+           <Sidebar/>
+        </aside>
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro est necessitatibus hic quia, ex alias nisi saepe asperiores omnis! Quaerat, totam quasi consequatur minima magnam delectus eius officiis cum. Minus!
+        </main>
+      </div>
     </>
   )
 }
